466ad918aef4d07cb94270215a950d3a83a5f21f
olfeeka <olfert90@gmail.com>
2023-11-06 17:44:21 +0400

    realization: validation of fields

--- card-english/src/components/WordsList/WordsList.jsx
+++ card-english/src/components/WordsList/WordsList.jsx
@@ -2,61 +2,77 @@ import { useState } from "react";
 import st from "./wordsList.module.scss";
 
 export default function WordsList(props) {
-  const { word, translation, transcription, category, id } = props;
+  const { word, translation, transcription, category } = props;
   const [isEdit, setEdit] = useState(false);
+  const [isEmpty, setEmpty] = useState(false); // состояние для проверки пустых полей
 
   const handleClick = () => {
     setEdit((isEdit) => !isEdit);
   };
 
+  const handleChange = (e) => { // функция обработчик изменения содержимого input полей
+    if (e.target.value === "") {
+      setEmpty(true);
+    } else {
+      setEmpty(false);
+    }
+  };
+
+  const lineClass = isEdit ? st.lineEdit : st.lineNotEdit;
+  const saveButtonClass = isEmpty ? st.btnSaveDisabled : st.btnSave;
+  const inputClass = isEmpty ? `${st.inputBox} ${st.error}` : st.inputBox;
+
   return (
     <>
-      {isEdit && (
-        <tr className={st.lineEdit}>
-          <td className={st.edit}>
-            <input type="text" defaultValue={word} />
-          </td>
-          <td className={st.edit}>
-            <input type="text" defaultValue={translation} />
-          </td>
-          <td className={st.edit}>
-            <input type="text" defaultValue={transcription} />
-          </td>
-          <td className={st.edit}>
-            <input type="text" defaultValue={category} />
-          </td>
-          <td className={st.edit}>
-            <button className={st.btnSave} onClick={handleClick}>
-              Save
-            </button>
-            <button className={st.btnCancel} onClick={handleClick}>
-              Cancel
-            </button>
-          </td>
-        </tr>
-      )}
-      {!isEdit && (
-        <tr className={st.lineNotEdit}>
-          <td className={st.notEdit}>
-            <input type="text" value={word} />
-          </td>
-          <td className={st.notEdit}>
-            <input type="text" value={translation} />
-          </td>
-          <td className={st.notEdit}>
-            <input type="text" value={transcription} />
-          </td>
-          <td className={st.notEdit}>
-            <input type="text" value={category} />
-          </td>
-          <td className={st.notEdit}>
-            <button className={st.btnEdit} onClick={handleClick}>
-              Edit
-            </button>
-            <button className={st.btnDelete}>Delete</button>
-          </td>
-        </tr>
-      )}
+      <tr className={lineClass}>
+        <td className={inputClass}>
+          <input
+            type="text"
+            defaultValue={word}
+            disabled={!isEdit}
+            onChange={handleChange}
+          />
+        </td>
+        <td className={inputClass}>
+          <input
+            type="text"
+            defaultValue={translation}
+            disabled={!isEdit}
+            onChange={handleChange}
+          />
+        </td>
+        <td className={inputClass}>
+          <input
+            type="text"
+            defaultValue={transcription}
+            disabled={!isEdit}
+            onChange={handleChange}
+          />
+        </td>
+        <td className={inputClass}>
+          <input
+            type="text"
+            defaultValue={category}
+            disabled={!isEdit}
+            onChange={handleChange}
+          />
+        </td>
+        <td className={st.inputBox}>
+          <button
+            className={isEdit ? saveButtonClass : st.btnEdit}
+            onClick={handleClick}
+            disabled={isEmpty}
+          >
+            {isEdit ? "Save" : "Edit"}
+          </button>
+          <button
+            className={isEdit ? st.btnCancel : st.btnDelete}
+            onClick={handleClick}
+          >
+            {isEdit ? "Cancel" : "Delete"}
+          </button>
+        </td>
+      </tr>
     </>
   );
 }
